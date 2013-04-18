55908a29e8ab1cff4370bc53bdf3cbd1046be362
peter.nochta@avangrade.com <peter.nochta@avangrade.com>
2013-04-18 04:45:15 -0500

    refactor views

--- KenoProjectManager/scripts/index.js
+++
@@ -1,9 +0,0 @@
-/*function login(){
-    app.showLoading();
-    setTimeout(function(){
-        $('.km-tabstrip').show();
-        app.navigate("projectList.html");
-        app.hideLoading();
-    },1000);
-}*/
-function reset(){}
--- KenoProjectManager/scripts/viewModel.js
+++ KenoProjectManager/scripts/viewModel.js
@@ -1,12 +1,17 @@
+
 var viewModel = kendo.observable({
 	loginName:null,
 	password:null,
 	stormProvider: null,
-	projectList: null,
-	selectedProject:null,
-	taskList:new kendo.data.DataSource(),
-	selectedTask:null,
+	
+    projectList: null,
+	taskList:null,
+    myTaskList:null,
 	workLogs:null,
+    
+    selectedProject:null,
+	selectedTask:null,
+
 	apiKey: {
 		ownerId: '952014ac-da7c-4024-93b2-644217ddba2c',
 		appId: 'ea20ec7c-6bd5-4db8-aa84-e62d789b2de5',
@@ -20,14 +25,20 @@ var viewModel = kendo.observable({
 		if (!credentials.user) {
 			credentials.user = 'admin';
 			credentials.password = 'admin';
+            this.loginName = 'admin';
+			this.password ='admin';
 		}
 		console.log(credentials);
 		app.showLoading();
 		$data.initService(this.apiKey, credentials).then(function(mydatabase, factory, type) {
 			viewModel.stormProvider = mydatabase;
 			viewModel.projectList = mydatabase.Projects.asKendoDataSource();
+            viewModel.myTaskList = mydatabase.Tasks.filter("it.Todo == this.UserName", {UserName:viewModel.loginName}).asKendoDataSource();
+            viewModel.taskList =  mydatabase.Tasks.asKendoDataSource();//mydatabase.Tasks.filter("it.Project_Id == this.pId", {pId:viewModel.selectedProjectId}).asKendoDataSource();
+            viewModel.workLogs =  mydatabase.WorkLogs.orderByDescending("it.CreateDate").asKendoDataSource();
+            
 			$('.km-tabstrip').show();
-			app.navigate("projectList.html");
+			app.navigate("views/projectList.html");
 		});
 	},
 	selectProject:function(e) {
@@ -44,12 +55,19 @@ var viewModel = kendo.observable({
 		app.navigate("#:back");
 	},
 	initializeTaskList:function() {
-		viewModel.set("taskList", viewModel.stormProvider.Tasks.filter("it.Project_Id == this.pId", {pId:viewModel.selectedProject.id}).asKendoDataSource());
+        viewModel.taskList.filter( { field: "Project_Id", operator: "equal", value: viewModel.selectedProject.id });
+	},
+    initializeMyTaskList:function() {
+		viewModel.myTaskList.read();
 	},
 	newTask:function() {
 		viewModel.set("selectedTask", viewModel.taskList.createItem());
-		viewModel.set("workLogs", viewModel.stormProvider.WorkLogs.filter("it.Task_Id == ''").asKendoDataSource());
+		viewModel.workLogs.filter({field:'Task_Id', operator:'equal', value:viewModel.selectedTask.id});
 		viewModel.selectedTask.set("Project_Id", viewModel.selectedProject.id);
+	},
+	selectTask:function(e) {
+		viewModel.set("selectedTask", e.dataItem);
+        viewModel.workLogs.filter({field:'Task_Id', operator:'equal', value:viewModel.selectedTask.id});
 	},
 	saveTask:function() {
 		if (viewModel.selectedTask.isNew()) {        
@@ -59,6 +77,7 @@ var viewModel = kendo.observable({
 			console.log("Save worklog if needed");
 			if ($('#taskDescription').val().length > 0) {
 				var workLog = viewModel.workLogs.createItem();
+                workLog.set('UserName', viewModel.loginName);
 				workLog.set('CreateDate', new Date());
 				workLog.set('Description', $('#taskDescription').val());
 				workLog.set('Task_Id', viewModel.selectedTask.id);
@@ -68,9 +87,5 @@ var viewModel = kendo.observable({
 			app.navigate("#:back");
 		});
 		viewModel.taskList.sync();
-	},
-	selectTask:function(e) {
-		viewModel.set("selectedTask", e.dataItem);
-		viewModel.set("workLogs", viewModel.stormProvider.WorkLogs.filter("it.Task_Id == this.tId", {tId:viewModel.selectedTask.id}).orderByDescending("it.CreateDate").asKendoDataSource());
 	}
 });
